a8b26e00a04a0391a455a6d1261ebd0a2adaeab1
Mohammed Karimi <mohammed.karimi01@hotmail.com>
2023-02-07 12:36:33 +0100

    Menu
    Started to fix the Enum menus for the diffrent options.

--- WeatherData/WeatherData/Methods.cs
+++ WeatherData/WeatherData/Methods.cs
@@ -9,6 +9,65 @@ namespace WeatherData
 {
     internal class Methods
     {
+        enum MainMenu
+        {
+            Inside = 1, 
+            Outside = 2
+        }
+        enum InsideMenu
+        {
+
+            Average_temperature_for_selected_date = 1,
+            Hottest_to_coldest,
+            Driest_to_moistest,
+            Least_to_greatest_risk_of_mold
+
+        }
+        enum OutsideMenu 
+        {
+            Average_temperature_and_humidity_per_day = 1,
+            Hottest_to_Coldest,
+            Driest_to_moistest,
+            Least_to_greatest_risk_of_mold,
+            Date_of_meteorological_Autumn,
+            Date_of_meteorological_winter
+        }
+        public static void RunMe()
+        {
+            Console.WriteLine("Welcome to Mohammed's & Thom's weatherdata app");
+            Console.WriteLine("Please choose one of the options below");
+            foreach (int c in Enum.GetValues(typeof(MainMenu)))
+            {
+                Console.WriteLine($"{c}: " + $"{Enum.GetName(typeof(MainMenu), c).Replace('_', ' ')}");
+            }
+
+            MainMenu mainmenu = (MainMenu)99;
+            int number;
+            if (int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out number))
+            {
+                mainmenu = (MainMenu)number;
+                Console.Clear();
+            }
+            switch (mainmenu) 
+            {
+                case MainMenu.Inside:
+                    Console.WriteLine("You are inside now");
+                    Inside();
+                    break;
+                case MainMenu.Outside:
+                    Console.WriteLine("You are outside now");
+                    Outside();
+                    break;
+            }
+        }
+        public static void Inside()
+        {
+
+        }
+        public static void Outside()
+        {
+
+        }
         public static void Pattern()
         {
             //  ([12]\d{ 3}-(0[6 - 9] | 1[0 - 2]) - (0[1 - 9] |[12]\d | 3[01]))     -TAR BORT MAJ
