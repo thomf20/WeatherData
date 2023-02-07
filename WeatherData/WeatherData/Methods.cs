838b35ce60e5078c65cf8dba614c9c4696f2d9cf
thomf20 <thom.flodqvist@gmail.com>
2023-02-07 11:09:03 +0000

    match test

---
+++ WeatherData/WeatherData/Methods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace WeatherData
+{
+    internal class Methods
+    {
+        public static void Pattern()
+        {
+            //  ([12]\d{ 3}-(0[6 - 9] | 1[0 - 2]) - (0[1 - 9] |[12]\d | 3[01]))     -TAR BORT MAJ
+            // (2016-05|2017-01)    -matcher det vi inte villmatcha
+            //      ^(2017-(0[2-9]|[2-9][0-9])|2016-(0[0-4]|0[6-9]|[1-9][0-9])).* - matchar det vi vill.
+        }
+        public static void CalculateMatches()
+        {
+            string path = "../../../../WeatherData/Files/TextFile1.txt";
+
+            Regex regex = new Regex("^(2017-(0[2-9]|[2-9][0-9])|2016-(0[0-4]|0[6-9]|[1-9][0-9])).*");
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string match in lines)
+            {
+                Console.WriteLine($"{match} matchar med {regex}: " + (regex.IsMatch(match) ? "Korrekt" : "Icke korrekt"));
+            }
+
+        }
+    }
+}
